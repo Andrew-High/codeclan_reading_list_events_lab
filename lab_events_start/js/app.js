b5b2ab4100d405811bb04d303b915447fb252bfd
Aaron McKie <aaron.mckie@icloud.com>
2021-05-06 15:40:29 +0100

    Entries ordered in list

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -12,8 +12,18 @@ const handleFormSubmit = function (event) {
   event.preventDefault();
   const resultFormInput = document.querySelector('#reading-list')
   const newListItem = document.createElement("li");
-  newListItem.textContent = `${event.target.title.value}${event.target.author.value}${event.target.category.value}`;
-  const list = document.querySelector("#reading-list")
+  const newUnorderedList = document.createElement("ul");
+  const newULListItem1 = document.createElement("li");
+  const newULListItem2 = document.createElement("li");
+  const newULListItem3 = document.createElement("li");
+  const list = document.querySelector("#reading-list");
+
+  // newListItem.textContent = `${event.target.title.value}${event.target.author.value}${event.target.category.value}`;
   list.appendChild(newListItem)
+  newListItem.appendChild(newUnorderedList)
+  newUnorderedList.appendChild(newULListItem1).textContent = `${event.target.title.value}`
+  newUnorderedList.appendChild(newULListItem2).textContent = `${event.target.author.value}`
+  newUnorderedList.appendChild(newULListItem3).textContent = `${event.target.category.value}`
+
   const clearForm = document.querySelector("#new-item-form").reset();
 }
